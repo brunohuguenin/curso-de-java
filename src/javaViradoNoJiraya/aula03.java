ce24f076b9bd12ed6096381e7b0c5eeceef766aa
brunohuguenin <brunohugueninn@gmail.com>
2024-02-28 19:57:11 -0300

    aula 03

---
+++ src/javaViradoNoJiraya/aula03.java
@@ -0,0 +1,20 @@
+package src.javaViradoNoJiraya;
+
+public class aula03 {
+    public static void main(String[] args) {
+
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero02/numero01;
+        System.out.println(resultado);
+
+        // % resto da divisão
+        int resto = 27 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        System.out.println("isDezMaiorQueVinte  " + isDezMaiorQueVinte);
+    }
+}
