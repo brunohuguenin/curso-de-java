af32c52a92435a8200d4ac9fe2dbe5e14d0bd888
brunohuguenin <brunohugueninn@gmail.com>
2024-03-03 19:41:12 -0300

    aula24

---
+++ JavaComLoianeGroner/Aulas/aula24/Carro.java
@@ -0,0 +1,9 @@
+package JavaComLoianeGroner.Aulas.aula24;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
---
+++ JavaComLoianeGroner/Aulas/aula24/TesteCarro.java
@@ -0,0 +1,24 @@
+package JavaComLoianeGroner.Aulas.aula24;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro van = new Carro();
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.capCombustivel= 100;
+        van.consumoCombustivel = 0.2;
+
+        System.out.printf("\nMarca do carro: %s\nModelo: %s\nNúmeros de passageiros: %d\n", van.marca, van.modelo, van.numPassageiros);
+
+        System.out.println("-----------------------------");
+
+        Carro fusca = new Carro();
+        fusca.marca = "Volkswagen";
+        fusca.modelo = "Fusca";
+        fusca.numPassageiros = 4;
+
+        System.out.printf("\nMarca do carro: %s\nModelo: %s\nNúmeros de passageiros: %d\n", fusca.marca, fusca.modelo, fusca.numPassageiros);
+
+    }
+}
