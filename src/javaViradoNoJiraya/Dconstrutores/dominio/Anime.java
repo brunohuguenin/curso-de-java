e7b68f6c82a919d5506fdf05b7ab2e584cde9b51
brunohuguenin <brunohugueninn@gmail.com>
2024-03-06 21:21:11 -0300

    aula 59

--- src/javaViradoNoJiraya/Dconstrutores/dominio/Anime.java
+++ src/javaViradoNoJiraya/Dconstrutores/dominio/Anime.java
@@ -5,14 +5,25 @@ public class Anime {
     private int episodios;
     private String nome;
     private String genero;
+    private  String estudio;
 
- 
+
     public Anime(String nome, String tipo, int episodios, String genero) {
-        System.out.println("Dentro do contrutor! :s");
+       // System.out.println("Dentro do contrutor! :s");
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+        System.out.println("DENTRO DO CONSTRUTOR SEM ARGUMENTO!!");
     }
 
     public void imprime(){
@@ -20,6 +31,7 @@ public class Anime {
         System.out.println(this.episodios);
         System.out.println(this.nome);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setTipo(String tipo) {
--- src/javaViradoNoJiraya/Dconstrutores/teste/AnimeTeste01.java
+++ src/javaViradoNoJiraya/Dconstrutores/teste/AnimeTeste01.java
@@ -4,7 +4,7 @@ import src.javaViradoNoJiraya.Dconstrutores.dominio.Anime;
 
 public class AnimeTeste01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Spike Lee", "Mangá", 12, "Adventure");
+        Anime anime = new Anime("Spike Lee", "Mangá", 12, "Adventure", "Universal");
         //anime.setGenero("Ação");
 
 //        anime.setTipo("Mangá");
