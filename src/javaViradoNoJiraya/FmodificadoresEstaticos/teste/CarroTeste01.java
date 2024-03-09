a17603310a1baf0dd38ae0a2e0e5b3124548666c
brunohuguenin <brunohugueninn@gmail.com>
2024-03-08 22:04:46 -0300

    aula 62

--- src/javaViradoNoJiraya/FmodificadoresEstaticos/teste/CarroTeste01.java
+++ src/javaViradoNoJiraya/FmodificadoresEstaticos/teste/CarroTeste01.java
@@ -4,14 +4,17 @@ import src.javaViradoNoJiraya.FmodificadoresEstaticos.domain.Carro;
 
 public class CarroTeste01 {
     public static void main(String[] args) {
-//        Carro carro1 = new Carro("BMW", 280);
-//        Carro carro2 = new Carro("Mercedes", 275);
-//        Carro carro3 = new Carro("Audi", 290);
-        System.out.println(Carro.velocidadeLimite);
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite);
-//        carro1.imprime();
-//        carro2.imprime();
-//        carro3.imprime();
+        Carro.setVelocidadeLimite(180);
+        Carro carro1 = new Carro("BMW", 280);
+        Carro carro2 = new Carro("Mercedes", 275);
+        Carro carro3 = new Carro("Audi", 290);
+
+//        System.out.println(Carro.velocidadeLimite);
+//        Carro.velocidadeLimite = 180;
+//        System.out.println(Carro.velocidadeLimite);
+
+        carro1.imprime();
+        carro2.imprime();
+        carro3.imprime();
     }
 }
