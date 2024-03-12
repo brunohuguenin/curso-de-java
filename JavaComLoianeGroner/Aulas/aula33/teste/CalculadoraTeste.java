2f6d3e2f7febe978b7f5fd2aedd11d405c0020ce
brunohuguenin <brunohugueninn@gmail.com>
2024-03-11 21:15:48 -0300

    aula 33

---
+++ JavaComLoianeGroner/Aulas/aula33/teste/CalculadoraTeste.java
@@ -0,0 +1,13 @@
+package JavaComLoianeGroner.Aulas.aula33.teste;
+
+import JavaComLoianeGroner.Aulas.aula33.dominio.Calculadora;
+
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        calc.soma(1, 2);
+
+        calc.soma(7.5, 6.3);
+    }
+}
